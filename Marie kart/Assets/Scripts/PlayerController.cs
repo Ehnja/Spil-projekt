e82c695f69d6cd73e1f059679278733321b405a5
Ehnja <hilmeroudijk@gmail.com>
2022-12-21 08:15:33 +0100

    Projekt mappe

---
+++ Marie kart/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    public float torque;
+    public float forceMultiplier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(0.0f, 0.0f, movementY) * forceMultiplier;
+        rb.AddRelativeForce(movement);
+        float turn = Input.GetAxis("Horizontal");
+        rb.AddRelativeTorque(Vector3.up * torque * turn);
+    }
+}
